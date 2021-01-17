e124d2be0adee40956d8082ccee29d399d937965
tyyuan <tyyuan@aibee.com>
2021-01-17 20:49:15 +0800

    fix成绩输出颜色问题

--- comment.py
+++ comment.py
@@ -4,7 +4,7 @@ version:
 Author: TianyuYuan
 Date: 2021-01-17 11:53:28
 LastEditors: TianyuYuan
-LastEditTime: 2021-01-17 18:34:10
+LastEditTime: 2021-01-17 20:46:50
 '''
 def intro():
     print("##############################")
@@ -14,7 +14,7 @@ def intro():
     print("\n")
     print("\033[1;32m->规则提示：\033[0m \
                 \n--胜利条件：一方棋子连成五子即胜利 \
-                \n--定义棋盘大小：棋盘边长应大于5且小于25 \
+                \n--定义棋盘大小：棋盘边长应大于5且小于20 \
                 \n--自定义棋子符号：一个字符的符号（也可使用默认棋子）\
                 \n--如何下棋：输入行号列号，并用逗号隔开 \
                 \n--退出游戏：输入‘q’即可退出游戏\n")
@@ -32,7 +32,7 @@ def congrats(winner:int,chess_piece:str):
         #############################\033[0m\n")
     print("\033[32m \
         >>>>玩家「{}」赢得了比赛！<<<<\n \
-        >>>>冠军的棋子：「{}」  <<<<\033[0m\n".format(winner,chess_piece))
+        >>>>冠军的棋子：「{}\033[32m」  <<<<\033[0m\n".format(winner,chess_piece))
 
 def quit_game():
     print("-> 退出游戏")
